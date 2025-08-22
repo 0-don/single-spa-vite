45c9ecdeabfd58202498b8fdf1d9ae354be93682
0-don <don.cryptus@gmail.com>
2025-08-22 21:11:38 +0200

    chore: refactor ESLint configuration to unify settings across packages

--- packages/root/eslint.config.js
+++ packages/root/eslint.config.js
@@ -4,25 +4,17 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-    },
-  }
-);
+export default tseslint.config(globalIgnores(["dist", "*.config.ts"]), {
+  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  files: ["src/**/*.{ts,tsx}"],
+  extends: [
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    reactHooks.configs["recommended-latest"],
+    reactRefresh.configs.vite,
+  ],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+  },
+});
